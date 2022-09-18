f7e71760a0cf8f0e283af21a3de6ef3e98fe8f84
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-18 16:18:36 +0300

    add video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,9 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
 player.on('play', function () {
   console.log('played the video!');
@@ -12,14 +14,28 @@ player.getVideoTitle().then(function (title) {
 });
 
 const onPlay = function (currentTime) {
-  console.log(currentTime);
   // data is an object containing properties specific to that event
-  const savedTime = localStorage.setItem(
-    'videoplayer-current-time',
-    'currentTime'
-  );
-  const parsed = JSON.parsed(savedTime);
-  console.log(parsed);
+  localStorage.setItem(STORAGE_KEY, currentTime.seconds);
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const timeOnStartPlay = localStorage.getItem(STORAGE_KEY);
+
+player
+  .setCurrentTime(timeOnStartPlay)
+  .then(function (seconds) {
+    console.log(seconds);
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
