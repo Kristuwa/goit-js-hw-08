2af6e3a54cf74b2de922cdb510891b5188403ba7
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-18 23:44:00 +0300

    add feedback task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,37 +8,48 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
+addEventListener('DOMContentLoaded', populateForm);
 
-const formData = {};
 const STORAGE_KEY = 'feedback-form-state';
-
-populateForm();
 
 function onFormSubmit(e) {
   e.preventDefault();
+  try {
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(formData);
+  } catch {
+    console.log('parsing error!');
+  }
 
-  console.log(formData);
-
-  e.target.reset();
+  e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
-  console.log(formData);
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  try {
+    const parsedData = savedMessage ? JSON.parse(savedMessage) : {};
+    parsedData.email = refs.email.value;
+    parsedData.message = refs.textarea.value;
+    //  console.log(parsedData);
+    const updateData = JSON.stringify(parsedData);
+    localStorage.setItem(STORAGE_KEY, updateData);
+  } catch {
+    console.log('parsing error');
+  }
 }
 
 function populateForm(e) {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const savedMessageNew = localStorage.getItem(STORAGE_KEY);
 
-  const parsedMessage = JSON.parse(savedMessage);
-
-  if (parsedMessage) {
-    refs.email.value = parsedMessage.email;
-    refs.textarea.value = parsedMessage.message;
-    console.log(parsedMessage.email);
-    console.log(parsedMessage.message);
+  if (savedMessageNew) {
+    try {
+      const parsedSavedMessage = JSON.parse(savedMessageNew);
+      refs.email.value = parsedSavedMessage.email;
+      refs.textarea.value = parsedSavedMessage.message;
+    } catch {
+      console.log('parsing error');
+    }
   }
 }
